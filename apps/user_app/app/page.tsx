cf9b15e890123101c79fe487dbb75c9639bffb0c
OjhaGopal <gopalojha7463@gmail.com>
2025-08-19 22:49:10 +0530

    add

--- apps/user_app/app/page.tsx
+++ apps/user_app/app/page.tsx
@@ -7,5 +7,18 @@ export default function Home() {
     <div>
       hi there
     </div>
+    <div>
+      <Image
+        src="/images/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        className={styles.logo}
+      />
+      <Button className={styles.button}>Click Me</Button>
+      <p className={styles.description}>
+        This is a simple example of a Next.js application.
+      </p>
+    </div>
   );
 }
